91740fb7ab167037cefbd608c41017b8ec03d0a9
angelogabitech <168835656+angelogabitech@users.noreply.github.com>
2024-05-24 20:39:27 -0300

    Enviando trabalho de Angelo + update de Lemos

--- main.js
+++ main.js
@@ -14,8 +14,8 @@
 //código utilizado pelo site do bingo.
 let listaCartelas = [
     [12345, [8, 4, 15, 3, 11, 30, 26, 19, 23, 18, 39, 43, 34, 41, 48, 56, 53, 49, 60, 63, 68, 74, 75, 71], 0],
-    [1986,  [7, 2, 11, 15, 10, 25, 26, 30, 17, 22, 35, 33, 45, 34, 60, 52, 47, 55, 56, 67, 62, 70, 71, 75], 0],
-    [1987,  [5, 15, 2, 8, 10, 23, 25, 30, 20, 17, 33, 34, 38, 45, 60, 47, 53, 50, 55, 62, 65, 68, 70, 75], 0]
+    [1986, [7, 2, 11, 15, 10, 25, 26, 30, 17, 22, 35, 33, 45, 34, 60, 52, 47, 55, 56, 67, 62, 70, 71, 75], 0],
+    [1987, [5, 15, 2, 8, 10, 23, 25, 30, 20, 17, 33, 34, 38, 45, 60, 47, 53, 50, 55, 62, 65, 68, 70, 75], 0],
 ]
 let listaCartelasProximasASeremBatidas = []
 let listaDeNumerosSorteados = []
@@ -26,7 +26,7 @@ function addNumeroListaSorteados() {
     if (domInputSorteado.value <= 1 || domInputSorteado.value >= 75) {
         alert("Numero Já foi sorteado")
         domInputSorteado.style.color = "red"
-    } else if (listaDeNumerosSorteados.includes(domInputSorteado.value)){
+    } else if (listaDeNumerosSorteados.includes(domInputSorteado.value)) {
         alert("Numero Já foi sorteado")
         domInputSorteado.setAttribute("class", "erro2")
     } else {
@@ -52,9 +52,19 @@ function atualizarCartelasProximasASeremBatidas() {
 //gama
 function exibirCartelasCadastradas() {
 
+
     //Selecionando o elemento do Seletor
     const seletor = document.getElementById("cartelas_id")
-    
+
+    //Criando opções a partir da quantidade de cartelas cadastradas
+    //    for (let contagem = 0; contagem < listaCartelas.length; contagem++) {
+    //        let option = document.createElement("option")
+    //        option.value = contagem + 1
+    //        option.textContent = contagem + 1
+    //        seletor.appendChild(option)
+    //    }
+
+
     //Mudar os números exibidos de acordo com o seletor 
     const container = document.getElementById("container");
 
@@ -99,11 +109,6 @@ function analisarSeACartelaVencedora() {
 
 }
 
-//zeta
-function analisarSeACartelaVencedora() {
-
-}
-
 
 //Separando a lista das tabelas
 qnt_cartelas = 0
@@ -112,9 +117,62 @@ qnt_cartelas_lista = []
 //Selecionando o elemento do Seletor
 const seletor = document.getElementById("cartelas_id")
 
+
 //Beta
 function cadastrarCartela() {
+    //Obtendo valores dos inputs
+    let inputCartelaID = document.getElementById("codCartela")
+    let inputNumerosCartela = document.getElementById("numerosDaCartela")
+
+    //Fazendo split e retirando espaços em branco, caso ocorra erros de digitação - ID e Números da Cartela
+    let NumerosCartela_SNULL = inputNumerosCartela.value.replace(/\s/g, "");;
+    let NumerosID_SNULL = inputCartelaID.value.replace(/\s/g, "");;
+
+    //alerta caso ocorra a digitação de alguma letra no ID e Cartela
+    //ID
+    inputCartelaID.addEventListener("input", function () {
+        if (/[a-zA-Z]/.test(inputCartelaID.value)) {
+            alert("O número de idenficacação da sua cartela, não deve conter letras!");
+            inputCartelaID.value = inputCartelaID.value.replace(/[a-zA-Z]/g, '');
+        }//
+    });
+    //CARTELA
+    inputNumerosCartela.addEventListener("input", function () {
+        if (/[a-zA-Z]/.test(inputNumerosCartela.value)) {
+            alert("Os números de sorteio da sua cartela, não devem conter letras!");
+            inputNumerosCartela.value = inputNumerosCartela.value.replace(/[a-zA-Z]/g, '');
+        }
+    });
+
+    console.log("O número de identificação da sua cartela é: " + NumerosID_SNULL, "Os números cadastrados foram: " + NumerosCartela_SNULL)
+
+    //Separando números por vírgulas
+    NumerosCartelaFINAL = NumerosCartela_SNULL.split(",")
+
+    LISTA_NUMEROS_INT = []
+
+    for (c = 0; c < NumerosCartelaFINAL.length; c++) {
+        LISTA_NUMEROS_INT.push(parseInt(NumerosCartelaFINAL[c]))
+    }
+    //Enviando ID e Números splitados para a lista de cartelas | 0 = Numeros sorteados
+    listaCartelas.push([parseInt(NumerosID_SNULL), LISTA_NUMEROS_INT, 0])
+
+    //Atualizando e inserindo novas opções ao seletor de cartelas
+    while (qnt_cartelas_lista.length < listaCartelas.length) {
+        qnt_cartelas = qnt_cartelas + 1
+        qnt_cartelas_lista.push(qnt_cartelas)
+        console.log("Temos essas cartelas: " + qnt_cartelas_lista)
+        //Adicionando opçoes ao select
+        let option = document.createElement("option")
+        option.value = parseInt(qnt_cartelas_lista.length)
+        option.textContent = parseInt(qnt_cartelas_lista.length)
+        seletor.appendChild(option)
+    }
+    //console.log(listaCartelas)
+
 }
+cadastrarCartela()
+exibirCartelasCadastradas()
 
 
 /////////////////////////////////////////////
